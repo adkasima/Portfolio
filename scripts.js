d3864c33902f3a4c931ca40218ba27ab495deb84
Adriel Kasima <adrielkasima@gmail.com>
2022-11-15 19:15:26 -0300

    add mobile menu

--- scripts.js
+++ scripts.js
@@ -1,1 +1,26 @@
-//error 404
+const mobileMenu = document.querySelector('.mobile-menu');
+const btnOpenMenu = document.querySelector(".btn-open-menu");
+const btnCloseMenu = document.querySelector(".btn-close-menu");
+
+btnOpenMenu.addEventListener('click', (e) => {
+    e.preventDefault();
+});
+
+btnCloseMenu.addEventListener('click', (e) => {
+    e.preventDefault();
+});
+
+
+function openMenu(){
+    mobileMenu.classList.remove("hide")
+    btnOpenMenu.classList.add("hide")
+    btnCloseMenu.classList.remove("hide")
+
+}
+
+function closeMenu() {
+    mobileMenu.classList.add("hide")
+    btnOpenMenu.classList.remove("hide")
+    btnCloseMenu.classList.add("hide")
+
+}
