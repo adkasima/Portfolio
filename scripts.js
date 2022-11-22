27930891d31dd83f694a90837f77b91053208c23
Adriel Kasima <adrielkasima@gmail.com>
2022-11-21 21:39:03 -0300

    new features

--- scripts.js
+++ scripts.js
@@ -2,11 +2,31 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const btnOpenMenu = document.querySelector(".btn-open-menu");
 const btnCloseMenu = document.querySelector(".btn-close-menu");
 
+function readMore(){
+    const points = document.getElementById("points");
+    const moreText = document.getElementById("more");
+    const btnReadMore = document.getElementById("btnReadMore");
+
+    if(points.style.display == "none"){
+        points.style.display = "inline";
+        moreText.style.display = "none";
+        btnReadMore.innerHTML = "Ler mais"
+    }else {
+        points.style.display = "none";
+        moreText.style.display = "inline";
+        btnReadMore.innerHTML = "Ler menos"
+    }
+}
+
 btnOpenMenu.addEventListener('click', (e) => {
     e.preventDefault();
 });
 
 btnCloseMenu.addEventListener('click', (e) => {
+    e.preventDefault();
+});
+
+btnReadMore.addEventListener('click', (e) => {
     e.preventDefault();
 });
 
